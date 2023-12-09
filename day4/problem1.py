1e25cb6e1de1fb57ef49e9800368b7e9a92280ed
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2023-12-09 16:50:06 +0100

    Day 4 solutions

---
+++ day4/problem1.py
@@ -0,0 +1,68 @@
+from typing import List, Tuple
+
+
+def get_matches(win_numbers: List[int], numbers: List[int]) -> int:
+    return len(set(win_numbers).intersection(set(numbers)))
+
+
+assert get_matches([1, 2, 3], [1]) == 1
+assert get_matches([1, 2, 3], [4]) == 0
+assert get_matches([1, 2, 3], [1, 2]) == 2
+assert get_matches([1, 2, 3], [1, 2, 3]) == 3
+assert get_matches([1, 2, 3], [1, 2, 2, 3]) == 3
+
+
+def mypower(to: int):
+    if to == 0:
+        return 1
+    elif to % 2 == 1:
+        val = mypower((to - 1) // 2)
+        return 2 * val * val
+    else:
+        val = mypower(to // 2)
+        return val * val
+
+
+assert mypower(0) == 1
+assert mypower(1) == 2
+assert mypower(2) == 4
+assert mypower(3) == 8
+
+
+def get_points(matches: int) -> int:
+    if matches == 0:
+        return 0
+    else:
+        return mypower(matches - 1)
+
+
+assert get_points(0) == 0
+assert get_points(1) == 1
+assert get_points(2) == 2
+assert get_points(3) == 4
+
+
+def parse_line(line: str) -> Tuple[List[int], List[int]]:
+    _, all_numbers = line.rsplit(":")
+    win_numbers, numbers = all_numbers.split("|")
+    win_numbers = list(map(int, win_numbers.strip().split()))
+    numbers = list(map(int, numbers.strip().split()))
+    return win_numbers, numbers
+
+
+assert parse_line("Card 1: 41 | 83") == ([41], [83])
+assert parse_line("Card 1: 41 24 | 83") == ([41, 24], [83])
+assert parse_line("Card 1: 41 24 | 83 25") == ([41, 24], [83, 25])
+
+
+def solution(fname: str):
+    total_points = 0
+    with open(fname) as f:
+        for line in f:
+            win_numbers, numbers = parse_line(line)
+            matches = get_matches(win_numbers, numbers)
+            total_points += get_points(matches)
+    return total_points
+
+
+print(solution("./input.txt"))
---
+++ day4/problem2.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple
+
+
+def get_matches(win_numbers: List[int], numbers: List[int]) -> int:
+    return len(set(win_numbers).intersection(set(numbers)))
+
+
+assert get_matches([1, 2, 3], [1]) == 1
+assert get_matches([1, 2, 3], [4]) == 0
+assert get_matches([1, 2, 3], [1, 2]) == 2
+assert get_matches([1, 2, 3], [1, 2, 3]) == 3
+assert get_matches([1, 2, 3], [1, 2, 2, 3]) == 3
+
+
+def update_cards(matches: List[int], cards: List[int]) -> List[int]:
+    n = len(cards)
+    for card_id in range(n - 1, -1, -1):
+        for next_card_id in range(card_id + 1, min(card_id + 1 + matches[card_id], n)):
+            cards[card_id] += cards[next_card_id]
+    return cards
+
+
+assert update_cards([0], [1]) == [1]
+assert update_cards([1, 0], [1, 1]) == [2, 1]
+assert update_cards([1, 1, 1], [1, 1, 1]) == [3, 2, 1]
+
+
+def parse_line(line: str) -> Tuple[List[int], List[int]]:
+    _, all_numbers = line.rsplit(":")
+    win_numbers, numbers = all_numbers.split("|")
+    win_numbers = list(map(int, win_numbers.strip().split()))
+    numbers = list(map(int, numbers.strip().split()))
+    return win_numbers, numbers
+
+
+assert parse_line("Card 1: 41 | 83") == ([41], [83])
+assert parse_line("Card 1: 41 24 | 83") == ([41, 24], [83])
+assert parse_line("Card 1: 41 24 | 83 25") == ([41, 24], [83, 25])
+
+
+def solution(fname: str) -> int:
+    matches = []
+    with open(fname) as f:
+        for line in f:
+            win_numbers, numbers = parse_line(line)
+            matches.append(get_matches(win_numbers, numbers))
+    cards = [1] * len(matches)
+    cards = update_cards(matches, cards)
+    return sum(cards)
+
+
+print(solution("./input.txt"))
