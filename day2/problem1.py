cf23c3919c44bdbcaac2659d1a9a6e06555df906
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2023-12-09 16:47:22 +0100

    Day 2 solutions

---
+++ day2/problem1.py
@@ -0,0 +1,49 @@
+from typing import Dict
+
+
+def is_valid_set(s, colors: Dict[str, int]) -> bool:
+    s = s.strip()
+    for num_with_color in s.split(","):
+        num_with_color = num_with_color.strip()
+        num, color = num_with_color.split(" ")
+        num = int(num)
+        color = color.strip()
+        if colors[color] < num:
+            return False
+    return True
+
+
+colors = {"red": 12, "green": 13, "blue": 14}
+assert is_valid_set("3 blue, 4 red", colors)
+assert is_valid_set("14 blue, 4 red", colors)
+assert not is_valid_set("15 blue, 4 red", colors)
+assert not is_valid_set("1 blue, 13 red", colors)
+
+
+def is_valid_game(sets, colors: Dict[str, int]) -> bool:
+    sets = sets.strip()
+    for s in sets.split(";"):
+        if not is_valid_set(s, colors):
+            return False
+    return True
+
+
+assert is_valid_game(" 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", colors)
+assert not is_valid_game(" 15 red", colors)
+assert not is_valid_game(
+    "8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", colors
+)
+
+
+def solution(input_file):
+    sum_ids = 0
+    colors = {"red": 12, "green": 13, "blue": 14}
+    with open(input_file) as f:
+        for line in f:
+            game_id, sets = line.split(":", 1)
+            if is_valid_game(sets, colors):
+                sum_ids += int(game_id.rsplit(" ", 1)[-1])
+    return sum_ids
+
+
+print(solution("input.txt"))
---
+++ day2/problem2.py
@@ -0,0 +1,80 @@
+from typing import Dict
+
+
+def get_set_colors(s) -> Dict[str, int]:
+    s = s.strip()
+    d = dict()
+    for num_with_color in s.split(","):
+        num_with_color = num_with_color.strip()
+        num, color = num_with_color.split(" ")
+        num = int(num)
+        color = color.strip()
+        d[color] = num
+    return d
+
+
+assert get_set_colors("3 blue") == {"blue": 3}
+assert get_set_colors("3 blue, 4 red") == {"red": 4, "blue": 3}
+assert get_set_colors("3 blue, 4 red, 5 green") == {"red": 4, "blue": 3, "green": 5}
+
+
+def update_colors(colors: Dict[str, int], new_colors: Dict[str, int]) -> Dict[str, int]:
+    for col, val in new_colors.items():
+        if col in colors:
+            colors[col] = max(colors[col], val)
+        else:
+            colors[col] = val
+    return colors
+
+
+assert update_colors({"green": 7}, {"blue": 10, "green": 5}) == {"green": 7, "blue": 10}
+assert update_colors({"green": 4}, {"blue": 10, "green": 5}) == {"green": 5, "blue": 10}
+assert update_colors({"green": 4}, {"blue": 10}) == {"green": 4, "blue": 10}
+
+
+def get_game_min_colors(sets) -> Dict[str, int]:
+    sets = sets.strip()
+    colors = dict()
+    for s in sets.split(";"):
+        new_colors = get_set_colors(s)
+        colors = update_colors(colors, new_colors)
+    return colors
+
+
+assert get_game_min_colors("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green") == {
+    "blue": 6,
+    "green": 2,
+    "red": 4,
+}
+assert get_game_min_colors(
+    "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+) == {
+    "blue": 4,
+    "green": 3,
+    "red": 1,
+}
+
+
+def mypower(colors: Dict[str, int]) -> int:
+    power = 1
+    for _, val in colors.items():
+        power *= val
+    return power
+
+
+assert mypower({"blue": 10}) == 10
+assert mypower({"blue": 10, "green": 2}) == 20
+assert mypower({"blue": 10, "green": 2, "red": 5}) == 100
+
+
+def solution(input_file):
+    sum_powers = 0
+    with open(input_file) as f:
+        for line in f:
+            _, sets = line.split(":", 1)
+            colors = get_game_min_colors(sets)
+            sum_powers += mypower(colors)
+    return sum_powers
+
+
+print(solution("./input.txt"))
